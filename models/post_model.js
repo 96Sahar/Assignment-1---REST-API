f2ad0d4ba075fc85a86d2723a70873b8bfd0f2e2
OriAbulafia <abulafiaori@gmail.com>
2024-11-17 09:37:56 +0200

    get all posts

--- models/post_model.js
+++ models/post_model.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const postsSchema = new Schema({
   username: String,
-  description: String,
+  description: {
+    type: String,
+    required: true,
+  },
   phone_number: String,
 });
 
-const Posts = mongoose.model("Posts", postsSchema);
-module.exports = Posts;
+const Post = mongoose.model("Post", postsSchema);
+module.exports = Post;
