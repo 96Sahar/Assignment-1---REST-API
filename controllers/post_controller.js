74db199b9329be220d153a01e3b37428f4f9e249
Sahar <96saharyosef@gmail.com>
2024-11-17 10:31:01 +0200

    created GetPostByID function

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -18,21 +18,34 @@ const getAllPosts = async (req, res) => {
 };
 
 const createPost = async (req, res) => {
-    console.log(req.body);
-    try {
-        const post = await Posts.create(req.body);  
-        if (post) {
-            return res.send(post);
-        }
-        else {
-            return res.status(404).send("Post not created");
-        }
-    } catch (err) {
-        return res.status(400).send(err.message);   
-    }       
+  console.log(req.body);
+  try {
+    const post = await Posts.create(req.body);
+    if (post) {
+      return res.send(post);
+    } else {
+      return res.status(404).send("Post not created");
+    }
+  } catch (err) {
+    return res.status(400).send(err.message);
+  }
 };
 
+const getPostById = async (req, res) => {
+  const id = req.params.id;
+  if (id) {
+    try {
+      const post = await Posts.findById(id);
+      return res.status(200).send(post);
+    } catch (error) {
+      return res.status(400).send(error.message);
+    }
+  } else {
+    console.log("Get Post By Id Error");
+  }
+};
 module.exports = {
-    getAllPosts,
-    createPost,
-  };
+  getAllPosts,
+  createPost,
+  getPostById,
+};
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -7,5 +7,6 @@ router.post("/", postController.createPost);
 
 router.get("/", postController.getAllPosts);
 
+router.get("/:id", postController.getPostById);
 
 module.exports = router;
